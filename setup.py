aec5afc4778d34043babe08f63f038c39a268c1d
Idan Gazit <idan@pixane.net>
2008-09-02 14:06:32 +0300

    modified setup to use "djangregator" not "Djangregator"

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ import ez_setup
 ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(
-    name = "Djangregator",
+    name = "djangregator",
     version = "0.1",
     packages = find_packages(),
     author = "Idan Gazit",
